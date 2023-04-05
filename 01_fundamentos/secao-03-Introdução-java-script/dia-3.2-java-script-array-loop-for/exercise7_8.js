8ac2706a91158945a221e375f3f39684b5f4377b
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-04-05 18:33:29 -0300

    Realizando exercício 7

--- 01_fundamentos/secao-03-Introdução-java-script/dia-3.2-java-script-array-loop-for/exercise7_8.js
+++ 01_fundamentos/secao-03-Introdução-java-script/dia-3.2-java-script-array-loop-for/exercise7_8.js
@@ -1,2 +1,20 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 // 7 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+
+let numerosImpar = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+
+    if (numbers[index] % 2 !== 0){
+        numerosImpar += 1;
+    }
+}
+
+if (numerosImpar === 0){
+    console.log('Nenhum valor ímpar encontrado');
+} else {
+    console.log(numerosImpar);
+}
+
 // 8 - Utilizando for, descubra o menor valor contido no array e imprima-o.
