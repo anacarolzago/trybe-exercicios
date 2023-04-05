8e065cb883653ce5ed209bede8b5b606a95d478f
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-04-05 18:39:55 -0300

    Realizando exercício 8

--- 01_fundamentos/secao-03-Introdução-java-script/dia-3.2-java-script-array-loop-for/exercise7_8.js
+++ 01_fundamentos/secao-03-Introdução-java-script/dia-3.2-java-script-array-loop-for/exercise7_8.js
@@ -4,17 +4,27 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let numerosImpar = 0;
 
-for (let index = 0; index < numbers.length; index += 1){
+for (let index = 0; index < numbers.length; index += 1) {
 
-    if (numbers[index] % 2 !== 0){
+    if (numbers[index] % 2 !== 0) {
         numerosImpar += 1;
     }
 }
 
-if (numerosImpar === 0){
+if (numerosImpar === 0) {
     console.log('Nenhum valor ímpar encontrado');
 } else {
     console.log(numerosImpar);
 }
 
 // 8 - Utilizando for, descubra o menor valor contido no array e imprima-o.
+
+let menorNumero = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[0] < menorNumero) {
+        menorNumero = numbers[index];
+    }
+}
+
+console.log(menorNumero);
