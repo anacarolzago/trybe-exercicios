3b7ec3621edbe3622ecd4016b7b459578843373c
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-05-09 20:11:12 -0300

    Fazendo exercicio 4

--- 01_fundamentos/secao-06-introducao-js-ES6-testes-unitarios/dia-6.2-primeiros-passos-jest/Exercicio-4/techList.test.js
+++ 01_fundamentos/secao-06-introducao-js-ES6-testes-unitarios/dia-6.2-primeiros-passos-jest/Exercicio-4/techList.test.js
@@ -1,10 +1,3 @@
-// A função techList recebe como parâmetros um array contendo uma lista de tecnologias e uma string com um nome. Para cada tecnologia no array a função cria, em ordem alfabética, um objeto com a seguinte estrutura:
-
-{
-    tech: 'nomeTecnologia',
-    name: name,
-};
-
 // Implemente a função techList a partir dos testes abaixo. É importante nunca alterar os testes ou as variáveis já escritas no código.
 
 const techList = require('./techList.js');
