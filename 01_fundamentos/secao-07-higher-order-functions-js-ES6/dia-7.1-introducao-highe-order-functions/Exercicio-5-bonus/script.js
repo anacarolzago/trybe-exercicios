6a781cd4831de9aea20e3f5f290a8c48c789d169
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-05-13 15:43:17 -0300

    Fazendo exercicio bonus

--- 01_fundamentos/secao-07-higher-order-functions-js-ES6/dia-7.1-introducao-highe-order-functions/Exercicio-5-bonus/script.js
+++ 01_fundamentos/secao-07-higher-order-functions-js-ES6/dia-7.1-introducao-highe-order-functions/Exercicio-5-bonus/script.js
@@ -30,18 +30,50 @@ const battleMembers = {
 };
 
 // Crie uma função que retorne o dano do dragão.
-
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+const dragonAttack = (dragon) => {
+    const minDmg = 15;
+    const maxDmg = Math.floor(Math.random() * dragon.strength);
+
+    const dragonDmg = maxDmg > minDmg ? maxDmg : minDmg;
+
+    return dragonDmg;
+};
 
 // Crie uma função que retorne o dano causado pelo warrior.
-
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+const warriorAttack = (warrior) => {
+    const minDmg = warrior.strength;
+    const maxDmg = Math.floor(Math.random() *(minDmg * warrior.weaponDmg));
+
+    const warriorDmg = maxDmg > minDmg ? maxDmg : minDmg;
+    return warriorDmg;
+};
 
 // Crie uma função que retorne um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo). // A mana consumida por turno é 15. Além disso, a função deve ter uma condicional: caso o mago tenha menos de 15 de mana, o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”), e a mana gasta é 0.
 
-// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+const mageAttack = (mage) => {
+    const mageMana = mage.mana;
+    const minDmg = mage.intelligence;
+    const maxDmg = minDmg * 2;
 
-// A mana consumida por turno é 15. Além disso, a função deve ter uma condicional: caso o mago tenha menos de 15 de mana, o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”), e a mana gasta é 0.
+    const turnStats = {
+      manaSpent: 0,
+      damageDealt: 'Not enough mana...',
+    };
+
+    if (mageMana > 15) {
+        const mageDamage = minDmg < maxDmg ? maxDmg : minDmg;
+        turnStats.manaSpent = 15;
+        turnStats.damageDealt = mageDamage;
+
+        return turnStats;
+    }
+
+    return turnStats;
+};
+
 
 // Parte II
 // Agora que você já possui a implementação das funções relativas aos três exercícios anteriores, passe-as como parâmetro para outras funções que irão compor um objeto gameActions. O objeto será composto por ações do jogo, e cada ação é por definição uma HOF, pois, nesse caso, são funções que recebem como parâmetro outra função.
@@ -49,8 +81,36 @@ const battleMembers = {
 // Copie o código abaixo e inicie sua implementação:
 
 const gameActions = {
-    // Crie as HOFs neste objeto.
+
+    warriorTurn: (warriorAttack) => {
+      const warriorDamage = warriorAttack(warrior);
+      warrior.damage = warriorDamage;
+      dragon.healthPoints -= warriorDamage;
+    },
+    
+    mageTurn: (mageAttack) => {
+      const mageTurnStats = mageAttack(mage);
+      const mageDamage = mageTurnStats.damageDealt;
+      mage.damage = mageDamage;
+      mage.mana -= mageTurnStats.manaSpent;
+      dragon.healthPoints -= mageDamage;
+    },
+
+    dragonTurn: (dragonAttack) => {
+      const dragonDamage = dragonAttack(dragon);
+      mage.healthPoints -= dragonDamage;
+      warrior.healthPoints -= dragonDamage;
+      dragon.damage = dragonDamage;
+    },
+
+    turnResults: () => battleMembers,
+    
 };
+  
+  gameActions.warriorTurn(warriorAttack);
+  gameActions.mageTurn(mageAttack);
+  gameActions.dragonTurn(dragonAttack);
+  console.log(gameActions.turnResults());
 
 // Crie a primeira HOF que compõe o objeto gameActions.
 // Ela será a função que simula o turno do personagem warrior. Essa HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon. Além disso, ela também deve atualizar o valor da chave damage do warrior.
