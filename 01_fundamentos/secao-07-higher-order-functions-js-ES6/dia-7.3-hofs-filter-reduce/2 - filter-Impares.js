2d657ed53aa9f1960321393f32f96e8d4311aaba
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-05-19 14:44:59 -0300

    Estudando reduce

--- 01_fundamentos/secao-07-higher-order-functions-js-ES6/dia-7.3-hofs-filter-reduce/2 - filter-Impares.js
+++ 01_fundamentos/secao-07-higher-order-functions-js-ES6/dia-7.3-hofs-filter-reduce/2 - filter-Impares.js
@@ -6,13 +6,14 @@ const arrayOfNumbers = [1, 5, 2, 25, 0];
 const filtraImpares = (arrayOfNumbers) => {
     // const arrayImpares = [];
 
-    // for (const elemento of arrayOfNumbers) {
-    //     if (elemento % 2 !== 0) {
-    //         arrayImpares.push(elemento);
+    // for (let index =0; index < arrayOfNumbers.length; index += 1) {
+    //     if (arrayOfNumbers[index] % 2 === 1 || arrayOfNumbers[index] % 2 === -1) {
+    //         arrayImpares.push(arrayOfNumbers[index]);
     //     }
     // }
+    // return arrayImpares;
 
     return arrayOfNumbers.filter((numero) => numero % 2 !== 0);
-}
+};
 
 module.exports = filtraImpares;
