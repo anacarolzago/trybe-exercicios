0e14f9401707c591a7a0bed6c6c4a4b9f8b1564c
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-05-30 20:27:41 -0300

    Exercicio na pratica

---
+++ 02_front-end/secao-02-introducao-react/dia-2.1-introducao-ao-react-e-ao-typescript/Exercicio2/hello-world/src/App.tsx
@@ -0,0 +1,13 @@
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <HelloWorld />
+      </div>
+    </>
+  );
+}
+
+export default App
