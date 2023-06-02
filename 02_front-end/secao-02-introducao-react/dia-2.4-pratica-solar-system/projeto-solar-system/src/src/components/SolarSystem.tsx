31b05c77177725caa93e70a70fd3e75db12bbf05
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-06-02 20:14:06 -0300

    Subindo exercicio solar system

---
+++ 02_front-end/secao-02-introducao-react/dia-2.4-pratica-solar-system/projeto-solar-system/src/src/components/SolarSystem.tsx
@@ -0,0 +1,29 @@
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+
+import planets from '../data/planets';
+
+function SolarSystem() {
+  return (
+    <div data-testid="solar-system">
+      <Title
+        headline="Planetas"
+      />
+
+      <ul>
+        {
+            planets.map((planet) => (
+              <li key={ planet.name }>
+                <PlanetCard
+                  planetName={ planet.name }
+                  planetImage={ planet.image }
+                />
+              </li>
+            ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default SolarSystem;
