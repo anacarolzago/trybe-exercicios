8acace488e368a11141ab67d8fc98c8c311a73de
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-06-07 20:23:11 -0300

    Refatorando o código para um Handler genérico para todos os inputs

--- 02_front-end/secao-02-introducao-react/dia-2.6-formularios/Course/src/App.tsx
+++ 02_front-end/secao-02-introducao-react/dia-2.6-formularios/Course/src/App.tsx
@@ -5,105 +5,213 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+// function App() {
+//   const [name, setName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [schooling, setSchooling] = useState('Médio');
+//   const [resume, setResume] = useState('');
+//   const [terms, setTerms] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   function resetForm() {
+//     setName('');
+//     setEmail('');
+//     setSchooling('Médio');
+//     setResume('');
+//     setTerms(false);
+//     setError(false);
+//   }
+
+//   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+//     event.preventDefault();
+
+//     if (terms) {
+//       alert(
+//         `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`,
+//       );
+//       resetForm();
+//     } else {
+//       setError(true);
+//     }
+//   }
+
+//   return (
+
+//   // Como iremos recuperar a chave target, podemos desestruturar o parâmetro da função: ({ target }) => setName(target.value). Essa alteração não é necessária, é apenas uma forma mais simples de escrever a mesma função
+//   // <>
+//   //   <label>
+//   //     Nome:
+//   //     <input value={ name } onChange={ (event) => setName(event.target.value) } />
+//   //   </label>
+//   //   <h1>Este é um paragrafo qualquer para dizer Olá, { name }!</h1>
+//   //   <button onClick={ () => setName('') }>Apagar!</button>
+//   // </>
+//     <>
+//       <form onSubmit={ (event) => handleSubmit(event) }>
+
+//         <label>
+//           Nome
+//           <input value={ name } onChange={ ({ target }) => setName(target.value) } />
+//         </label>
+
+//         <label>
+//           E-mail
+//           <input value={ email } onChange={ ({ target }) => setEmail(target.value) } />
+//         </label>
+
+//         <label>
+//           Escolaridade
+
+//           <select
+//             value={ schooling }
+//             onChange={ ({ target }) => setSchooling(target.value) }
+//           >
+
+//             <option value="Médio">Médio</option>
+//             <option value="Superior">Superior</option>
+//             <option value="Pós-graduação">Pós-graduação</option>
+//           </select>
+
+//         </label>
+
+//         <label>
+//           Resumo das experiências
+
+//           <textarea
+//             value={ resume }
+//             onChange={ ({ target }) => setResume(target.value) }
+//           />
+//         </label>
+
+//         <label>
+//           Aceito os termos e condições
+
+//           <input
+//             type="checkbox"
+//             checked={ terms }
+//             onChange={ () => setTerms((prevTerms) => !prevTerms) }
+//           />
+//         </label>
+
+//         <button>Enviar</button>
+
+//       </form>
+
+//       { error && (
+//         <h4>
+//           Você precisa aceitar os termos e condições para poder enviar o
+//           currículo
+//         </h4>
+//       )}
+
+//     </>
+//   );
+// }
+
+// Refatorando: função handler para todos os inputs
+
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [schooling, setSchooling] = useState('Médio');
-  const [resume, setResume] = useState('');
   const [terms, setTerms] = useState(false);
   const [error, setError] = useState(false);
+  const [formInfo, setFormInfo] = useState({
+    name: '',
+    email: '',
+    schooling: 'Médio',
+    resume: '',
+  });
 
   function resetForm() {
-    setName('');
-    setEmail('');
-    setSchooling('Médio');
-    setResume('');
-    setTerms(false);
-    setError(false);
+    setFormInfo({
+      name: '',
+      email: '',
+      schooling: 'Médio',
+      resume: '',
+    });
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
     if (terms) {
-      alert(
-        `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`,
-      );
       resetForm();
+      setTerms(false);
+      setError(false);
+      alert(
+        `Nome: ${formInfo.name}\nEmail: ${formInfo.email}\nEscolaridade: ${formInfo.schooling}\nExperiências: ${formInfo.resume}`,
+      );
     } else {
       setError(true);
     }
   }
 
+  function handleChange(
+    event: React.ChangeEvent<
+    HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  }
+
   return (
-
-  // Como iremos recuperar a chave target, podemos desestruturar o parâmetro da função: ({ target }) => setName(target.value). Essa alteração não é necessária, é apenas uma forma mais simples de escrever a mesma função
-  // <>
-  //   <label>
-  //     Nome:
-  //     <input value={ name } onChange={ (event) => setName(event.target.value) } />
-  //   </label>
-  //   <h1>Este é um paragrafo qualquer para dizer Olá, { name }!</h1>
-  //   <button onClick={ () => setName('') }>Apagar!</button>
-  // </>
     <>
-      <form onSubmit={ (event) => handleSubmit(event) }>
-
+      <form onSubmit={ handleSubmit }>
         <label>
-          Nome
-          <input value={ name } onChange={ ({ target }) => setName(target.value) } />
+          Nome:
+          <input
+            required
+            name="name"
+            value={ formInfo.name }
+            onChange={ handleChange }
+          />
         </label>
-
         <label>
-          E-mail
-          <input value={ email } onChange={ ({ target }) => setEmail(target.value) } />
+          E-mail:
+          <input
+            required
+            name="email"
+            value={ formInfo.email }
+            onChange={ handleChange }
+          />
         </label>
-
         <label>
-          Escolaridade
-
+          Escolaridade:
           <select
-            value={ schooling }
-            onChange={ ({ target }) => setSchooling(target.value) }
+            name="schooling"
+            value={ formInfo.schooling }
+            onChange={ handleChange }
           >
-
             <option value="Médio">Médio</option>
             <option value="Superior">Superior</option>
             <option value="Pós-graduação">Pós-graduação</option>
           </select>
-
         </label>
-
         <label>
           Resumo das experiências
-
           <textarea
-            value={ resume }
-            onChange={ ({ target }) => setResume(target.value) }
+            name="resume"
+            value={ formInfo.resume }
+            onChange={ handleChange }
           />
         </label>
-
         <label>
           Aceito os termos e condições
-
           <input
             type="checkbox"
             checked={ terms }
-            onChange={ () => setTerms((prevTerms) => !prevTerms) }
+            onChange={ () => setTerms(!terms) }
           />
         </label>
-
         <button>Enviar</button>
-
       </form>
-
-      { error && (
+      {error && (
         <h4>
           Você precisa aceitar os termos e condições para poder enviar o
           currículo
         </h4>
       )}
-
     </>
   );
 }
