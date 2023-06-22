a01bcad446c056633450a5393965ede398d340b3
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-06-22 15:41:52 -0300

    useEffect na pŕatica: consumindo uma API

--- 02_front-end/secao-03-aprofundando-no-react/dia-3.2-useEffect/Course/src/App.tsx
+++ 02_front-end/secao-03-aprofundando-no-react/dia-3.2-useEffect/Course/src/App.tsx
@@ -1,10 +1,46 @@
+import { useState, useEffect } from 'react';
+import { fetchCoordinates } from './services';
+
 import './App.css';
 
+// Passo 1 - Crie o estado do componente, que deverá armazenar um objeto que contém as chaves latitude e longitude. Crie o tipo Coordinates para indicar ao TypeScript o que se espera como valor do estado:
+
+// Passo 2 - Importar a função fetchCoordinates criada anteriormente, a fim de que você tenha acesso aos valores retornados pela API e armazene-os no estado coordinates.
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
 function App() {
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  // O objetivo é que a requisição seja feita apenas uma vez, assim que o componente for renderizado. Portanto, o useEffect deve ter dois parâmetros:
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, []);
+
+  // Está sendo criada a função assíncrona fetchData dentro do useEffect. Essa função vai aguardar o retorno de fetchCoordinates e armazená-lo no estado do componente. Na sequência, execute fetchData.
+
   return (
     <>
+      <h1>International Space Station Location Tracker</h1>
 
+      { /* Passo 3 - Agora que os valores estão armazenados no estado do componente, basta renderizá-los. Porém, é preciso ter cuidado com um detalhe! Antes de executar o useEffect, o componente é renderizado pela primeira vez. Porém, nesse momento, o valor do estado coordinates ainda é vazio. Portanto, a fim de que a aplicação funcione corretamente, é necessário verificar se o estado já estará preenchido com as informações: */ }
+
+      {coordinates && (
+        <>
+          <h2>{ `Latitude: ${coordinates.latitude}` }</h2>
+          <h2>{ `Longitude: ${coordinates.longitude}` }</h2>
+        </>
+      )}
     </>
+
   );
 }
 
